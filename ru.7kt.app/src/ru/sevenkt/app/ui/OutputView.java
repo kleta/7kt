058ba3a07718a4fe4771d2e5e25b2d3144ae357b
zaharov <zaharov@khakensb.ru>
2016-03-21 16:51:25 +0700

    'AutoCommit'

--- ru.7kt.app/src/ru/sevenkt/app/ui/OutputView.java
+++ ru.7kt.app/src/ru/sevenkt/app/ui/OutputView.java
@@ -1,13 +1,25 @@
- 
 package ru.sevenkt.app.ui;
 
+ 
+
+
 import javax.inject.Inject;
+
+import java.util.List;
+
 import javax.annotation.PostConstruct;
+
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.SWT;
+import org.slf4j.LoggerFactory;
 
-public class OutputView {
+import ch.qos.logback.classic.Logger;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.Appender;
+import ch.qos.logback.core.AppenderBase;
+
+public class OutputView{
 	private Text text;
 	@Inject
 	public OutputView() {
@@ -15,13 +27,10 @@ public class OutputView {
 	}
 	
 	@PostConstruct
-	public void postConstruct(Composite parent) {
-		
-		text = new Text(parent, SWT.BORDER | SWT.WRAP | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CANCEL | SWT.MULTI);
-		
-	}
-	
-	
-	
-	
+	public void postConstruct(Composite parent) {	
+//		Logger l = (ch.qos.logback.classic.Logger) LoggerFactory.getLogger("");
+//		List<Logger> app = l.getLoggerContext().getLoggerList();
+//		System.out.println(l.getClass());
+		text = new Text(parent, SWT.BORDER | SWT.WRAP | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CANCEL | SWT.MULTI);	
+	}	
 }
