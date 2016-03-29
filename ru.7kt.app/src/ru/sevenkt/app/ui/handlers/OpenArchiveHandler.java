4778258940f6922e3a3b1f4a58fe8aa6468d874e
zaharov <zaharov@khakensb.ru>
2016-03-29 14:38:11 +0700

    89: выводить данные в приращениях
    Task-Url: http://abakannew:3000/issues/89

--- ru.7kt.app/src/ru/sevenkt/app/ui/handlers/OpenArchiveHandler.java
+++ ru.7kt.app/src/ru/sevenkt/app/ui/handlers/OpenArchiveHandler.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -33,6 +34,7 @@ import ru.sevenkt.db.entities.Measuring;
 import ru.sevenkt.db.services.IDBService;
 import ru.sevenkt.domain.ArchiveTypes;
 import ru.sevenkt.domain.Parameters;
+import ru.sevenkt.domain.ParametersConst;
 
 public class OpenArchiveHandler implements EventHandler {
 
@@ -74,12 +76,22 @@ public class OpenArchiveHandler implements EventHandler {
 		Device device = (Device) event.getProperty(AppEventConstants.DEVICE);
 		LocalDate startDate = (LocalDate) event.getProperty(AppEventConstants.START_DATE);
 		LocalDateTime startDateTime = startDate.atStartOfDay();
-		if(archiveType.equals(ArchiveTypes.MONTH) && startDateTime.getDayOfMonth()!=1)
-			startDateTime=startDateTime.plusMonths(1).withDayOfMonth(1);
+		if (archiveType.equals(ArchiveTypes.MONTH) && startDateTime.getDayOfMonth() != 1)
+			startDateTime = startDateTime.plusMonths(1).withDayOfMonth(1);
 		LocalDate endDate = (LocalDate) event.getProperty(AppEventConstants.END_DATE);
 		LocalDateTime endDateTime = endDate.atStartOfDay();
 
 		Map<String, Object> result = new HashMap<>();
+		switch (archiveType) {
+		case MONTH:
+			startDate=startDate.minusMonths(1);
+			break;
+		case DAY:
+			startDate=startDate.minusDays(1);
+			break;
+		default:
+			break;
+		}
 		List<Measuring> measurings = dbService.findArchive(device, startDate, endDate, archiveType);
 		Map<LocalDateTime, List<Measuring>> groupByDateTime = measurings.stream()
 				.collect(Collectors.groupingBy(Measuring::getDateTime));
@@ -89,25 +101,54 @@ public class OpenArchiveHandler implements EventHandler {
 		groupByParameters.keySet().iterator().forEachRemaining(parameters::add);
 		List<TableRow> listTableRow = new ArrayList<>();
 		do {
-			List<Measuring> lm = groupByDateTime.get(startDateTime);
 			TableRow tr = new TableRow();
 			tr.setDateTime(startDateTime);
-			if (lm != null)
-				for (Measuring measuring : lm) {
-					tr.getValues().put(measuring.getParameter(), measuring.getValue());
-				}
-			listTableRow.add(tr);
+			List<Measuring> lm = groupByDateTime.get(startDateTime);
 			switch (archiveType) {
 			case MONTH:
+				List<Measuring> lmPrevMonth = groupByDateTime.get(startDateTime.minusMonths(1));
+				if (lmPrevMonth != null && lm != null) {
+					for (int i = 0; i < lmPrevMonth.size(); i++) {
+						Measuring measuring = lm.get(i);
+						String category = measuring.getParameter().getCategory();
+						if (!category.equals(ParametersConst.TEMP) && !category.equals(ParametersConst.PRESSURE)) {
+							Measuring prevMonthMeasuring = lmPrevMonth.get(i);
+							if(measuring.getParameter().equals(prevMonthMeasuring.getParameter()) && prevMonthMeasuring.getValue()<=measuring.getValue())
+								tr.getValues().put(measuring.getParameter(), measuring.getValue()-prevMonthMeasuring.getValue());
+						}
+						else
+							tr.getValues().put(measuring.getParameter(), measuring.getValue());
+					}
+				}
+				listTableRow.add(tr);
 				startDateTime = startDateTime.plusMonths(1);
 				break;
 			case DAY:
+				List<Measuring> lmPrevDay = groupByDateTime.get(startDateTime.minusDays(1));
+				if (lmPrevDay != null && lm != null) {
+					for (int i = 0; i < lmPrevDay.size(); i++) {
+						Measuring measuring = lm.get(i);
+						String category = measuring.getParameter().getCategory();
+						if (!category.equals(ParametersConst.TEMP) && !category.equals(ParametersConst.PRESSURE)) {
+							Measuring prevDayMeasuring = lmPrevDay.get(i);
+							if(measuring.getParameter().equals(prevDayMeasuring.getParameter()) && prevDayMeasuring.getValue()<=measuring.getValue())
+								tr.getValues().put(measuring.getParameter(), measuring.getValue()-prevDayMeasuring.getValue());
+						}
+						else
+							tr.getValues().put(measuring.getParameter(), measuring.getValue());
+					}
+				}
+				listTableRow.add(tr);
 				startDateTime = startDateTime.plusDays(1);
 				break;
 			case HOUR:
+				if (lm != null)
+					for (Measuring measuring : lm) {
+						tr.getValues().put(measuring.getParameter(), measuring.getValue());
+					}
+				listTableRow.add(tr);
 				startDateTime = startDateTime.plusHours(1);
 				break;
-
 			default:
 				break;
 			}
