7cfcfb0baeb7e57dc097f4d7391f67a1edb6a83c
zaharov <zaharov@khakensb.ru>
2016-04-05 16:51:11 +0700

    'AutoCommit'

---
+++ ru.7kt.app/src/ru/sevenkt/app/ui/handlers/JournalSettingsHandler.java
@@ -0,0 +1,13 @@
+
+package ru.sevenkt.app.ui.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+
+public class JournalSettingsHandler {
+
+	@Execute
+	public void execute() {
+		System.out.println();
+	}
+
+}
