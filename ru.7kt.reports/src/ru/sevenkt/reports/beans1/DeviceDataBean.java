7cfdfd9b33a8aad0ceb07f84a2b21161c17d0408
zaharov <zaharov@khakensb.ru>
2016-05-27 11:04:25 +0700

    js

---
+++ ru.7kt.reports/src/ru/sevenkt/reports/beans1/DeviceDataBean.java
@@ -0,0 +1,19 @@
+package ru.sevenkt.reports.beans1;
+
+import java.util.List;
+
+public class DeviceDataBean {
+	
+	private String name;
+	
+	private String serialNum;
+	
+	private String dateFrom;
+	
+	private String dateTo;
+	
+	private List<MeterBean> meters;
+	
+	private List<ConsumptionBean> consumptions;
+
+}
---
+++ ru.7kt.reports/src/ru/sevenkt/reports/beans1/DeviceDataBeanFactory.java
@@ -0,0 +1,13 @@
+package ru.sevenkt.reports.beans1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DeviceDataBeanFactory {
+	
+	public static List<DeviceDataBean> create(){
+		
+		return new ArrayList<DeviceDataBean>();
+		
+	}
+}
